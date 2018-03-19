80903247ae51ac3dea83327c8aaf8c504889d6c4
BrunoFFari <bruno27ffarias@gmail.com>
2018-03-19 20:37:16 -0300

    Aula 4

--- app1/urls.py
+++ app1/urls.py
@@ -13,14 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
 from django.conf.urls import url
-from . import views
-from app1.views import ola
+from app1.views import *
 
 urlpatterns = [
+    url('', index),
     path('admin/', admin.site.urls),
-    url(r'^ola/$', ola),
-    url(r'^$', ola),
 ]
--- app1/views.py
+++ app1/views.py
@@ -1,5 +1,5 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
-def ola(request):
-    return  HttpResponse("Olá mundo!")
-   
+def index(request):
+    return render(request, "index.html")
